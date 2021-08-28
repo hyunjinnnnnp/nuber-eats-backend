061b841672ea643165282a1f9e03950b5fa87a66
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-08-28 15:08:36 +0900

    TypeORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jini',
+      password: '12345', //localhost경우 필요없음
+      database: 'nuber-eats',
+      synchronize: true, //true: TypeORM이 데이터베이스에 연결할 때 데이터베이스를 내 모듈의 현재상태로 마이그래이션한다
+      logging: true, //데이터베이스 상태 콘솔출력
+    }),
   ],
   controllers: [],
   providers: [],
