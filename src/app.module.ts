9b41c399b76a3b7f087b553ed92ad20babb321e1
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-08-28 15:38:03 +0900

    Configuring ConfigService(dotenv)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD, //localhost경우 필요없음
+      database: process.env.DB_NAME,
+      synchronize: true, //true: TypeORM이 데이터베이스에 연결할 때 데이터베이스를 내 모듈의 현재상태로 마이그래이션한다
+      logging: true, //데이터베이스 상태 콘솔출력
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     RestaurantsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'jini',
-      password: '12345', //localhost경우 필요없음
-      database: 'nuber-eats',
-      synchronize: true, //true: TypeORM이 데이터베이스에 연결할 때 데이터베이스를 내 모듈의 현재상태로 마이그래이션한다
-      logging: true, //데이터베이스 상태 콘솔출력
-    }),
   ],
   controllers: [],
   providers: [],
