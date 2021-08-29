2f2871ab92d733e17e4060a864d27c9577efad94
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-08-29 21:33:14 +0900

    Update Restaurant: arguments. dto

---
+++ src/restaurants/dtos/update-restaurant.dto.ts
@@ -0,0 +1,16 @@
+import { Field, InputType, PartialType } from '@nestjs/graphql';
+import { createRestaurantDto } from './create-restaurant.dto';
+
+@InputType()
+export class UpdateRestaurantInputType extends PartialType(
+  createRestaurantDto,
+) {}
+
+@InputType()
+export class UpdateRestaurantDto {
+  @Field((type) => Number)
+  id: number;
+
+  @Field((type) => UpdateRestaurantInputType)
+  data: UpdateRestaurantInputType;
+}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restuarants.service';
 
@@ -24,10 +25,11 @@ export class RestaurantResolver {
       return false;
     }
   }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    return true;
+  }
 }
-//instead of these, use ArgsType! => create.restaurant.dto.ts
-//createRestaurant():
-// @Args('isVegan') isVegan: boolean,
-// @Args('name') name: string,
-// @Args('address') address: string,
-// @Args('ownersName') ownersName: string
