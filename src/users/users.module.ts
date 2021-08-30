c36abdcc1c8ef6d69c21e8d6599fe3e7f569fa29
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-08-30 13:51:35 +0900

    User Resolver and  Service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  //import for injecting repository
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersResolver, UsersService],
+})
 export class UsersModule {}
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,14 @@
+import { Query } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver((of) => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query((returns) => Boolean)
+  hi() {
+    return true;
+  }
+}
