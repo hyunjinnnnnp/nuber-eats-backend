d760a8a001e49bef1e91b61b23b2adc46e04a769
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-02 22:14:19 +0900

    Mailgun API

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -127,6 +127,7 @@ export class UsersService {
       if (verification) {
         verification.user.verified = true;
         this.users.save(verification.user);
+        this.verification.delete(verification.id);
         return { ok: true };
       }
       return { ok: false, error: 'Verification not found.' };
