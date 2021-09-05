fa58bf7e414dfa1e4e9d7d388d481bfa9a15ebc3
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-05 14:49:53 +0900

    e2e test: login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -10,11 +10,17 @@ jest.mock('got', () => {
   };
 });
 
+const testUser = {
+  email: 'nico@las.com',
+  password: '12345',
+};
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 //Users Resolver test
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,7 +37,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'nico@las.com';
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -39,8 +44,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation{
             createAccount(input:{
-              email: "${EMAIL}",
-              password:"12345",
+              email: "${testUser.email}",
+              password:"${testUser.password}",
               role:Owner
             }){
               ok
@@ -55,15 +60,16 @@ describe('UserModule (e2e)', () => {
         });
     });
 
-    it('should fail if account already exist', () => {
+    it('should fail if account already exists', () => {
+      //afterAll --> database.drop 이기때문에 순차적으로 진행시킨다.
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
         .send({
           query: `
           mutation{
             createAccount(input:{
-              email: "${EMAIL}",
-              password:"12345",
+              email: "${testUser.email}",
+              password:"${testUser.password}",
               role:Owner
             }){
               ok
@@ -81,8 +87,69 @@ describe('UserModule (e2e)', () => {
     });
   });
 
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            login(input:{
+              email:"${testUser.email}",
+              password: "${testUser.password}",
+            }){
+              ok
+              token
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          console.log(res);
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            login(input:{
+              email:"${testUser.email}",
+              password: "wrongPassword",
+            }){
+              ok
+              token
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toBe(null);
+        });
+    });
+  });
+
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
