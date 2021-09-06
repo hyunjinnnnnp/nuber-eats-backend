17f8c014eb4891265452c2267d87820b1887027c
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-06 16:54:35 +0900

    createRestaurant: create and save category & Slug

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restuarants.service';
 
 @Module({
   //import for injecting repository
-  imports: [TypeOrmModule.forFeature([Restaurant])],
+  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
   providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
