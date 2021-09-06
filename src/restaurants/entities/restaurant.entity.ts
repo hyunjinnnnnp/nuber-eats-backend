2ddefd3895c2d29be045b91552c1fe27c47d07e1
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-06 17:32:59 +0900

    Role based Authorization: SetMetadata with role

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -34,8 +34,7 @@ export class Restaurant extends CoreEntity {
 
   @Field((type) => User)
   @ManyToOne((type) => User, (user) => user.restaurants, {
-    nullable: true,
-    onDelete: 'SET NULL',
+    onDelete: 'CASCADE',
   })
   owner: User;
 }
